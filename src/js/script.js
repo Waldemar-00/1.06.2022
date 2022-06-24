e5a87cdd3d22a321eaef2bc394bea6c1148a28f5
uladzimir <for_intention@outlook.com>
2022-06-24 21:23:36 +0300

    add modal windows

--- src/js/script.js
+++ src/js/script.js
@@ -16,14 +16,12 @@ $(document).ready(function(){
       {
         breakpoint: 992,
         settings: {
-          // slidesToShow: 3,
-          // slidesToScroll: 3,
-          // infinite: true,
           dots: true,
           arrows: false,
         }
       },],
 });
+//MOD CLASS
 function toggleSlide(item) {
   $(item).each(function(i){
      $(this).on('click', function(e) {
@@ -35,42 +33,29 @@ function toggleSlide(item) {
 };
 toggleSlide('.catalog-item__link');
 toggleSlide('.catalog-item__back');
+//MODAL WINDOWS
+$('[data-modal=consultation]').on('click', function() {
+  $('.overlay, #consultation').fadeIn('slow');
+});
+$('.modal__close').on('click', function() {
+  $('.overlay, #consultation, #thanks, #order ').fadeOut('slow');
+});
+// $('.button_mini').on('click', function() {
+//   $('.overlay, #order').fadeIn('slow');
+// });
+$('.button_mini').each(function(i) {
+  $(this).on('click', function() {
+    $('#order .modal__description').text($('.catalog-item__subtitle').eq(i).text());
+    $('.overlay, #order').fadeIn('slow');
+  });
+});
  });
+ //TABS
  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
     $(this)
       .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
       .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
- // $('.catalog-item__link').each(function(i){
- //    $(this).on('click', function(e) {
- //      e.preventDefault();
- //      $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
- //      $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
- //    });
- // });
 });
 document.querySelector('::marker').addEventListener('click', function () {
     marker.style.backgroundColor = grey;
  });
-//TINY SLIDER !!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// const slider = tns({
-//     container: '.carousel__inner',
-//     items: 1,
-//     slideBy: 'page',
-//     autoplay: false,
-//     controls: false,
-//     nav: false,
-//     speed: 1200,
-//
-//   });
-//   document.querySelector('.prev').onclick = function () {
-//   slider.goTo('prev');
-// };
-// document.querySelector('.next').onclick = function () {
-// slider.goTo('next');
-// };
-// document.querySelector('.prev').addEventListener('click', function () {
-// slider.goTo('prev');
-// });
-// document.querySelector('.next').addEventListener('click', function () {
-// slider.goTo('next');
-// });
