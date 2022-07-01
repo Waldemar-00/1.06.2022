849b4916a56bea3c00de04fe16009b5d6353d78c
uladzimir <83595316+Waldemar-00@users.noreply.github.com>
2022-07-01 20:49:11 +0300

    delete trash & add masked

--- src/js/script.js
+++ src/js/script.js
@@ -71,6 +71,13 @@ function valideForms(form) {
 valideForms('#first-form');
 valideForms('#main-Form');
 valideForms('#main-Order');
+$('input[name=phone]').mask("+7 (999) 999-99-99");
+// jQuery(function($){
+//    $("#date").mask("99/99/9999");
+//    $("#phone").mask("(999) 999-9999");
+//    $("#tin").mask("99-9999999");
+//    $("#ssn").mask("999-99-9999");
+// });
  });
  //TABS
  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
